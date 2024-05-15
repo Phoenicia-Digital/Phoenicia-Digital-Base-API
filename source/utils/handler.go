35b84e23932b4b827c0c47906b0263026347ab84
Maxim Sbeyti <maximsby77@gmail.com>
2024-05-16 00:36:19 +0300

    Add Readable Comments in source/utils/handler.go for readability

--- source/utils/handler.go
+++ source/utils/handler.go
@@ -6,25 +6,36 @@ import (
 	"net/http"
 )
 
+// The Interface we use for our functions | Since they return an error
+// used in http.Handle(_, <PhoeniciaDigitalHandler(function)>)
 type PhoeniciaDigitalHandler func(http.ResponseWriter, *http.Request) error
 
 type ApiError struct {
+	// ApiError struct can be returned as a `val` in `SendJSON` so we get a JSON response
+	// for unsucessful calls || ERRORS
 	Code       int `json:"status"`
 	ErrorQuote any `json:"error"`
 }
 
 type ApiSuccess struct {
+	// ApiSuccess struct can be returned as a `val` in `SendJSON` so we get a JSON response
+	// for successful calls
 	Code         int `json:"status"`
 	SuccessQuote any `json:"success"`
 }
 
 func SendJSON(w http.ResponseWriter, status int, val any) error {
+	// Set the Response Writers Header status and the content type to JSON so that we can send JSON
 	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Encode The Value `val` into the Response Writer and return an error if occured which will be managed by
+	// the ServeHTTP interface ONLY IF USING `PhoeniciaDigitalHnadler`
 	return json.NewEncoder(w).Encode(val)
 }
 
+// Implementaion of the http.ServeHTTP interface on `PhoeniciaDigitalHandler`
 func (pdf PhoeniciaDigitalHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Set CORS headers
 	w.Header().Set("Access-Control-Allow-Origin", "*")
